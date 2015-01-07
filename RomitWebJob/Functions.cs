6f292614a856b895851feedf637d3d20f27e5df0
shawnweisfeld <shawn@shawnweisfeld.com>
2015-01-07 15:54:15 -0600

    with ef

---
+++ RomitWebJob/Functions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.WebJobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomitWebJob
+{
+    public class Functions
+    {
+        public static void WriteMessage([QueueTrigger("romitmessagequeue")] string message)
+        {
+            Console.WriteLine(message);
+
+            using (var db = new RomitMessagesEntities())
+            {
+                db.QueueMessages.Add(new QueueMessage()
+                    {
+                        MessageBody = message,
+                        MessageOn = DateTime.UtcNow
+                    });
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ RomitWebJob/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.WebJobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomitWebJob
+{
+    // To learn more about Microsoft Azure WebJobs, please see http://go.microsoft.com/fwlink/?LinkID=401557
+    class Program
+    {
+        static void Main()
+        {
+            JobHost host = new JobHost();
+            host.RunAndBlock();
+        }
+
+        private static void Initialize()
+        {
+            Console.WriteLine("Initialize Start.");
+
+
+            Console.WriteLine("Initialize Finish.");
+        }
+
+
+    }
+}
